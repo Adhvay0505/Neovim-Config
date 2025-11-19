f0004e72fb8287724829a5912fc0ee8a0e719ccf
./adhv4y <149415126+Adhvay0505@users.noreply.github.com>
2025-11-20 03:25:55 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -9,12 +9,17 @@ vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
 
+-- Statusline colors - bright highlighter green background with black text
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "#CCFF00", fg = "#000000", bold = false })
+vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#B8E600", fg = "#000000", bold = false })
+vim.api.nvim_set_hl(0, "StatusLineBold", { bg = "#CCFF00", fg = "#000000", bold = true })
+
 -- Basic settings
 vim.opt.number = true                              -- Line numbers
 vim.opt.relativenumber = true                      -- Relative line numbers
 vim.opt.cursorline = true                          -- Highlight current line
 vim.opt.wrap = false                               -- Don't wrap lines
-vim.opt.scrolloff = 10                             -- Keep 10 lines above/below cursor
+vim.opt.scrolloff = 10                             -- Keep 10 lines above/below cursor 
 vim.opt.sidescrolloff = 8                          -- Keep 8 columns left/right of cursor
 
 -- Indentation
@@ -28,7 +33,7 @@ vim.opt.autoindent = true                          -- Copy indent from current l
 -- Search settings
 vim.opt.ignorecase = true                          -- Case insensitive search
 vim.opt.smartcase = true                           -- Case sensitive if uppercase in search
-vim.opt.hlsearch = false                           -- Don't highlight search results
+vim.opt.hlsearch = false                           -- Don't highlight search results 
 vim.opt.incsearch = true                           -- Show matches as you type
 
 -- Visual settings
@@ -38,15 +43,15 @@ vim.opt.colorcolumn = ""                        -- Show column at 100 characters
 vim.opt.showmatch = true                           -- Highlight matching brackets
 vim.opt.matchtime = 2                              -- How long to show matching bracket
 vim.opt.cmdheight = 1                              -- Command line height
-vim.opt.completeopt = "menuone,noinsert,noselect"  -- Completion options
-vim.opt.showmode = false                           -- Don't show mode in command line
-vim.opt.pumheight = 10                             -- Popup menu height
-vim.opt.pumblend = 10                              -- Popup menu transparency
-vim.opt.winblend = 0                               -- Floating window transparency
-vim.opt.conceallevel = 0                           -- Don't hide markup
-vim.opt.concealcursor = ""                         -- Don't hide cursor line markup
+vim.opt.completeopt = "menuone,noinsert,noselect"  -- Completion options 
+vim.opt.showmode = false                           -- Don't show mode in command line 
+vim.opt.pumheight = 10                             -- Popup menu height 
+vim.opt.pumblend = 10                              -- Popup menu transparency 
+vim.opt.winblend = 0                               -- Floating window transparency 
+vim.opt.conceallevel = 0                           -- Don't hide markup 
+vim.opt.concealcursor = ""                         -- Don't hide cursor line markup 
 vim.opt.lazyredraw = true                          -- Don't redraw during macros
-vim.opt.synmaxcol = 300                            -- Syntax highlighting limit
+vim.opt.synmaxcol = 300                            -- Syntax highlighting limit 
 
 -- File handling
 vim.opt.backup = false                             -- Don't create backup files
@@ -327,7 +332,7 @@ local function FloatingTerminal()
   terminal_state.is_open = true
   vim.cmd("startinsert")
 
-  -- Set up auto-close on buffer leave
+  -- Set up auto-close on buffer leave 
   vim.api.nvim_create_autocmd("BufLeave", {
     buffer = terminal_state.buf,
     callback = function()
@@ -520,10 +525,6 @@ _G.git_branch = git_branch
 _G.file_type = file_type
 _G.file_size = file_size
 
-vim.cmd([[
-  highlight StatusLineBold gui=bold cterm=bold
-]])
-
 -- Function to change statusline based on window focus
 local function setup_dynamic_statusline()
   vim.api.nvim_create_autocmd({"WinEnter", "BufEnter"}, {
@@ -544,7 +545,7 @@ local function setup_dynamic_statusline()
     }
     end
   })
-  vim.api.nvim_set_hl(0, "StatusLineBold", { bold = true })
+  vim.api.nvim_set_hl(0, "StatusLineBold", { bg = "#CCFF00", fg = "#000000", bold = true })
 
   vim.api.nvim_create_autocmd({"WinLeave", "BufLeave"}, {
     callback = function()
